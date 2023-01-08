0416a680edac0f2a42cf4645ab442d54ce996614
Maxime Wojtczak <maxime.wojtczak@zenika.com>
2023-01-08 16:22:33 +0100

    feat: add group and common labels for templating

--- scripts/local/listen.sh
+++ scripts/local/listen.sh
@@ -4,5 +4,5 @@ set -ex
 
 rm -rf /var/db/net-snmp
 mkdir -p $HOME/.snmp/mibs
-ln -s $PWD/../mibs/SNMP-NOTIFIER-MIB.my $HOME/.snmp/mibs/SNMP-NOTIFIER-MIB.my
-snmptrapd -m ALL -m +SNMP-NOTIFIER-MIB -f -Of -Lo -c snmptrapd.conf -x 127.0.0.1 "::1"
+ln -s $PWD/../mibs/SNMP-NOTIFIER-MIB.my $HOME/.snmp/mibs/SNMP-NOTIFIER-MIB.my || true
+snmptrapd -m ALL -m +SNMP-NOTIFIER-MIB -f -Of -Lo -c snmptrapd.conf
