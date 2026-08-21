3ad0b60c0675ba8262b4792340cf434014193424
Maxime Wojtczak <maxime.wojtczak@yahoo.fr>
2026-08-22 00:50:52 +0200

    ci: update promci (#310)
    * ci: update promci
    
    * chore: update licence header
    
    * deps: bump golang to 1.26

--- telemetry/telemetry.go
+++ telemetry/telemetry.go
@@ -1,4 +1,4 @@
-// Copyright 2018 Maxime Wojtczak
+// Copyright 2026 Maxime Wojtczak
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
